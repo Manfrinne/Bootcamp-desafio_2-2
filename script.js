da8b5df0458b5da28f1d7d36f5b02fb01a12828f
Manfrinne Ferreira <m4nfrinne@gmail.com>
2020-05-27 23:01:20 -0300

    add maximize botton in the Modal

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
+const modalMax = document.querySelector('.modal')
 
 for (let card of cards) {
     card.addEventListener("click", function() {
@@ -14,4 +15,13 @@ for (let card of cards) {
 document.querySelector('.close-modal').addEventListener("click", function() {
     modalOverlay.classList.remove('active');
     modalOverlay.querySelector('iframe').src = "";
+    modalMax.classList.remove('fullscreen');
+})
+
+document.querySelector('.max-modal').addEventListener("click", function() {
+    if (modalMax.classList.contains('fullscreen')) {
+        modalMax.classList.remove('fullscreen');
+    } else {
+        modalMax.classList.add('fullscreen');
+    }
 })
